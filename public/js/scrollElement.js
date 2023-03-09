593e747cd3d93dcd6dca65d8d18e2e034dbef1ee
clementine777 <darklord778.aldo@gmail.com>
2023-03-09 18:48:51 -0300

    solucionaddo scroll de seccion contactme

--- public/js/scrollElement.js
+++ public/js/scrollElement.js
@@ -2,10 +2,8 @@ var botones = document.querySelectorAll("li");
 
 for (var i = 0; i < botones.length; i++) {
   botones[i].addEventListener("click", function (event) {
-    // Obtener la información de la sección a la que se debe hacer scroll
     var seccion = this.getAttribute("data-scroll");
 
-    // Hacer scroll a la sección correspondiente
     var seccionPos = document.querySelector(seccion).offsetTop;
 
     window.scrollTo({
